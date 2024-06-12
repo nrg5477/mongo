fb033364f1feec75169c97efddeea6074666bebc
nrg5477 <nrg5477@naver.com>
2024-06-12 10:20:19 +0900

    미완성2

--- src/main/java/com/example/mongo/dto/ScoreDTO.java
+++ src/main/java/com/example/mongo/dto/ScoreDTO.java
@@ -1,8 +1,12 @@
 package com.example.mongo.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Getter
+@Setter
 public class ScoreDTO {
 
 }
